3d6e880306ed7944b1ffc4d64822a60389fbe183
unohayato <mamidori1p1p@gmail.com>
2022-09-25 03:02:37 +0900

    dotenv

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-const request = require("request")
+const request = require("request");
+const dotenv = require("dotenv").config();
 
 const options = {
-  url: "https://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid=cfc6f11fba2e4b41e874fa5f9be3bffc",
+  url: `https://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid=${process.env.API_KEY}`,
   method: "GET",
   json: true,
 

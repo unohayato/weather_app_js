8ce5fadf250c293f50c9ec5065077479c3a2b622
unohayato <mamidori1p1p@gmail.com>
2022-09-24 20:37:14 +0900

    セルシウスでapiをget

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const request = require("request")
 
 const options = {
-  url: "https://api.openweathermap.org/data/2.5/weather?q=London&appid=cfc6f11fba2e4b41e874fa5f9be3bffc",
+  url: "https://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid=cfc6f11fba2e4b41e874fa5f9be3bffc",
   method: "GET",
   json: true,
 

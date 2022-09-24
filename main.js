df05c1b459fe3fe80e06eb583c1043f76b0e3531
unohayato <mamidori1p1p@gmail.com>
2022-09-24 20:35:08 +0900

    get api

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log("Hello");
+const request = require("request")
+
+const options = {
+  url: "https://api.openweathermap.org/data/2.5/weather?q=London&appid=cfc6f11fba2e4b41e874fa5f9be3bffc",
+  method: "GET",
+
+};
+
+request(options, (error, res, body) => {
+  console.log(body);
+});

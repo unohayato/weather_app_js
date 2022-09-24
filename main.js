bd8124800e79ed9046613bab13cd36f6310de569
unohayato <mamidori1p1p@gmail.com>
2022-09-25 03:05:19 +0900

    get temp in any place

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 const request = require("request");
 const dotenv = require("dotenv").config();
 
+const argment = process.argv[2];
+
 const options = {
-  url: `https://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid=${process.env.API_KEY}`,
+  url: `https://api.openweathermap.org/data/2.5/weather?q=${argment}&units=metric&appid=${process.env.API_KEY}`,
   method: "GET",
   json: true,
 
@@ -11,3 +13,5 @@ const options = {
 request(options, (error, res, body) => {
   console.log(body.main.temp);
 });
+
+console.log(process.argv[2]);
